c6a0b36a3bc2764919e5f15657111f080396b97c
Julian Steinwachs <steiwachs-it@posteo.de>
2022-06-16 10:31:04 +0200

    initial commit with backend from spring initilizr

---
+++ backend/src/main/kotlin/de/steinwachsit/reactivealltheway/ReactiveallthewayApplication.kt
@@ -0,0 +1,11 @@
+package de.steinwachsit.reactivealltheway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveallthewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveallthewayApplication>(*args)
+}
---
+++ backend/src/test/kotlin/de/steinwachsit/reactivealltheway/ReactiveallthewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.steinwachsit.reactivealltheway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveallthewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
